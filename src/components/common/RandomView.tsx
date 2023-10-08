e8787d7bc5bb7cac0da286685f9a1a0974ee1ba6
kasiwamotchi <totopomu@gamil.com>
2023-10-08 16:26:04 +0900

    add:ページに表示する画面の雛形を作成しました
    yarn devしたら表示されたのでRandomView.tsxに追加していきます

---
+++ src/components/common/RandomView.tsx
@@ -0,0 +1,11 @@
+
+const RandomView = () => {
+  return (
+    <div>
+      <button>ボタン</button>
+      <div>画像が出る</div>
+    </div>
+  )
+}
+
+export default RandomView
---
+++ src/pages/topPage.tsx
@@ -0,0 +1,10 @@
+import RandomView from 'src/components/common/RandomView'
+const topPage = () => {
+  return (
+    <div>
+      <RandomView/>
+    </div>
+  )
+}
+
+export default topPage
