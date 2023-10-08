91cbf1614fe685a76ae7f0bdbd6fec03d6746dda
kasiwamotchi <totopomu@gamil.com>
2023-10-08 21:20:39 +0900

    refact:フォーマッターをかけました
    空白の追加やセミコロンの削除などがされています

--- src/components/common/RandomView.tsx
+++ src/components/common/RandomView.tsx
@@ -1,14 +1,21 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 const touchImage = () => {
-  return `https://source.unsplash.com/random`;
+  return `https://source.unsplash.com/random`
 }
 
 const RandomView = () => {
   return (
     <div>
       <button>ボタン</button>
-      <Image className="w-full" loader={touchImage} src="random" width={500} height={500} alt="ランダムで表示される画像" />
+      <Image
+        className='w-full'
+        loader={touchImage}
+        src='random'
+        width={500}
+        height={500}
+        alt='ランダムで表示される画像'
+      />
     </div>
   )
 }
--- src/pages/topPage.tsx
+++ src/pages/topPage.tsx
@@ -2,7 +2,7 @@ import RandomView from 'src/components/common/RandomView'
 const topPage = () => {
   return (
     <div>
-      <RandomView/>
+      <RandomView />
     </div>
   )
 }
